9189638113ea1c8e0ded9602ba3f8e3aac4f32e9
Andy07cama <47961283@est.ort.edu.ar>
2024-05-28 09:08:49 -0300

    Final Incompleto
    casi terminado

---
+++ Assets/Login.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    public string Contra = "Moneda";
+    public InputField IngresoContra; 
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Contraseña()
+    {
+     if (IngresoContra.text == Contra)
+        {
+            Debug.Log("Access granted");
+        }
+     else
+        {
+            Debug.Log("Access denied");
+        }
+    }
+}
